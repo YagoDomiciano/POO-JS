5cc4208382c83d1359c45e8eb37f4fc33aa2309c
YAGO HENRIQUE MARTINS DOMICIANO <26214@unifacef.edu.br>
2026-05-21 20:38:52 -0300

    Matriz espersa

--- 08-complexidadeA.mjs
+++ 08-complexidadeA.mjs
@@ -1,78 +1,126 @@
-const array = [5,6,7,8,9,2,3,4,1]
-
-function exemplo1(array){
-    const inicio = performance.now()
-    if (array.length === 0) return null
-    const elemento = array[3]
-    const fim =  performance.now()
-
-    const memoriaPilhaMB = (array.length * 4) / (1024 * 1024)
-
-    let memoriaTotalMB = "N/A"
-    if(typeof process !== "undefined" && process.memoryUsage){
-        memoriaTotalMB = process.memoryUsage().heapUsed / 1024 /1024
+// O(1), porque a operação de acesso ao primeiro elemento do array é feita em tempo constante, independentemente do tamanho do array
+function exampleO1(arr1) {
+    const inicio = performance.now();
+    if (arr1.length === 0) return null;
+    const primeiroElemento = arr1[50];
+    const fim = performance.now();
+    const memoriaPilhaMB = (arr1.length * 4) / (1024 * 1024);
+  
+    let memoriaTotalMB = "N/A";
+    if (typeof process !== "undefined" && process.memoryUsage) {
+      memoriaTotalMB = process.memoryUsage().heapUsed / 1024 / 1024;
     }
-    console.clear()
-    console.log("--- Analise de complexidade iterativa O(1) ---")
-    console.log("Tamanho entrada ", array.length)
-    console.log("Resultado ", elemento)
-    console.log("Tempo de execução ", fim - inicio, "MS")
-    console.log("Memoria estimada da pilha de chamada ", memoriaPilhaMB, " MB")
-    console.log("Memoria total usada pelo processo ", 
-        typeof memoriaTotalMB === "number" ? memoriaTotalMB.toFixed(6) + " MB" : memoriaPilhaMB)
-}
-
-exemplo1(array)
-
-function exemploLogN(n){
-    let i = 1
-    while (i < n){
-        console.log(i)
-        i *= 2
-
-    }
-}
-
-exemploLogN(10)
-
-function exemploOn(array){
-    for(let i = 0; i < array.length; i++){
-        console.log(array[i])
-    }
-}
-
-const dados = Array(60).fill(9)
-exemploOn(array)
-
-function exemploLogN(array){
-    array.sort() // é um O(n Log N)
-    for(let i=0; i<array.length; i++){
-
-        console.log(array[i])
-    }
-}
-
-function exemploN_2(array){
-    for(let i = 0; i < array.length; i++){
-        for(let j = 0; j < array.length; j++){
-            console.log(array[i], array[j])
-        }
-    }
-}
-
-function exemplo2_N(n){
-    if(n === 0) return 1
-    return exemplo2_N(n - 1) + exemplo2_N(n - 1)
-}
-
-function factorialIterativo(n){
-    let resultado = 1
-    for(let i = n; i > 1; i--){
-        resultado = resultado * i
-    }
-}
-
-function fatorialRecursivo(num) {
-    if (num === 0) return 1;
-    return num * fatorialRecursivo(num - 1);
+    console.clear();
+    console.log("---Análise de complexidade Iterativa O(1)---");
+    console.log("Tamanho da entrada: ", arr1.length);
+    console.log("Resultado: ", primeiroElemento);
+    console.log("Tempo de Execução: ", fim - inicio, " ms");
+    console.log("Memória estimada da pilha de chamadas: ", memoriaPilhaMB, " MB");
+    console.log(
+      "Memória total usada pelo processo: ",
+      typeof memoriaTotalMB === "number"
+        ? memoriaTotalMB.toFixed(6) + " MB"
+        : memoriaTotalMB,
+    );
   }
+  
+  // const arr1 = [5, 7, 8, 9, 2, 3, 4];
+  
+  let dados = Array.from({length: 10000}, () => Math.random())
+  // exampleO1(dados);
+  
+  
+  
+  // O(log n), porque a cada iteração, o valor de i é multiplicado por 2, o que significa que o número de iterações necessárias para que i atinja "n" é logarítimico em relação a "n"
+  function exemploOlogN(n) {
+    const inicio = performance.now();
+    let i = 1;
+    while (i < n) {
+      i *= 2;
+    }
+    const fim = performance.now();
+    const memoriaPilhaMB = (n * 4) / (1024 * 1024);
+  
+    let memoriaTotalMB = "N/A";
+    if (typeof process !== "undefined" && process.memoryUsage) {
+      memoriaTotalMB = process.memoryUsage().heapUsed / 1024 / 1024;
+    }
+    console.clear();
+    console.log("---Análise de complexidade Logarítmica O(log n)---");
+    console.log("Tamanho da entrada: ", n);
+    console.log("Resultado: ", i);
+    console.log("Tempo de Execução: ", fim - inicio, " ms");
+    console.log("Memória estimada da pilha de chamadas: ", memoriaPilhaMB, " MB");
+    console.log(
+      "Memória total usada pelo processo: ",
+      typeof memoriaTotalMB === "number"
+        ? memoriaTotalMB.toFixed(6) + " MB"
+        : memoriaTotalMB,
+    );
+  }
+  // exemploOlogN(5);
+  
+  
+  
+  // O(n), porque a função percorre todos os elementos do array um vez, realizando uma operação constante para cada elemento (console.log)
+  function exemploOn(arr2) {
+    const inicio = performance.now();
+    for (let i = 0; i < arr2.length; i++) {
+      arr2[i]
+    }
+  
+    const fim = performance.now();
+    const memoriaPilhaMB = (arr2.length * 4) / (1024 * 1024);
+  
+    let memoriaTotalMB = "N/A";
+    if (typeof process !== "undefined" && process.memoryUsage) {
+      memoriaTotalMB = process.memoryUsage().heapUsed / 1024 / 1024;
+    }
+    console.clear();
+    console.log("---Análise de complexidade Linear O(n)---");
+    console.log("Tamanho da entrada: ", arr2.length);
+    console.log("Resultado: ", arr2);
+    console.log("Tempo de Execução: ", fim - inicio, " ms");
+    console.log("Memória estimada da pilha de chamadas: ", memoriaPilhaMB, " MB");
+    console.log(
+      "Memória total usada pelo processo: ",
+      typeof memoriaTotalMB === "number"
+        ? memoriaTotalMB.toFixed(6) + " MB"
+        : memoriaTotalMB,
+    );
+  }
+  
+  const arr2 = [1, 2, 3, 4, 5];
+  exemploOn(dados);
+  
+  // O(n^2), porque a função contém dois loops aninhados que percorrem todos os elentos do array, resultando em um número total de operações proporcional ao quadrado do tamanho do array
+  function exemploN2(arr3) {
+    const inicio = performance.now();
+    for (let i = 0; i < arr3.length; i++) {
+      for (let j = 0; j < arr3.length; j++) {
+          arr3[i], arr3[j]
+      }
+    }
+    const fim = performance.now();
+    const memoriaPilhaMB = (arr3.length * 4) / (1024 * 1024);
+  
+    let memoriaTotalMB = "N/A";
+    if (typeof process !== "undefined" && process.memoryUsage) {
+      memoriaTotalMB = process.memoryUsage().heapUsed / 1024 / 1024;
+    }
+    console.clear();
+    console.log("---Análise de complexidade Quadrática O(n^2)---");
+    console.log("Tamanho da entrada: ", arr3.length);
+    console.log("Resultado: ", arr3, arr3);
+    console.log("Tempo de Execução: ", fim - inicio, " ms");
+    console.log("Memória estimada da pilha de chamadas: ", memoriaPilhaMB, " MB");
+    console.log(
+      "Memória total usada pelo processo: ",
+      typeof memoriaTotalMB === "number"
+        ? memoriaTotalMB.toFixed(6) + " MB"
+        : memoriaTotalMB,
+    );
+  }
+  
+  const arr3 = [1, 2, 3, 4, 5];
+  // exemploN2(arr3);
